149ddb2af2cd82dde15a2d1887d0b0655d9bbafb
minji Song <ssong758@gmail.com>
2020-07-13 17:56:02 +0900

    add route for upload

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import account from './account';
+import upload from './upload';
 
 const router = express.Router();
-router.use('/account', account);
 
 router.use('/*', (req, res, next) => {
     res.setHeader("Expires", "-1");
     res.setHeader("Cache-Control", "must-revalidate, private");
     next();
 });
+
+router.use('/account', account);
+router.use('/upload', upload);
 
 export default router;
