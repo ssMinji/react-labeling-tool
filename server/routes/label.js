29fca7d9874b7e6c18ed8d352905bb25d7297f86
minji Song <ssong758@gmail.com>
2020-07-17 17:52:28 +0900

    async/await for map function of sql query

--- server/routes/label.js
+++ server/routes/label.js
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { response } from 'express';
 import { getConnection } from '../main';
+import { resolve } from 'path';
 
 const router = express.Router();
 
@@ -19,7 +20,6 @@ router.get('/', (req, res) => {
 });
 
 router.post('/doLabel', (req, res) => {
-    // console.log(req.body.files);
     let categoryCode;
     let labelCode;
     let comment = req.body.files.comment;
@@ -54,40 +54,87 @@ router.post('/doLabel', (req, res) => {
     EXPERT SIDE
     GET /api/getLabel
 */
+
+async function getNameByCode(result, conn) {
+    return new Promise((resolve, reject) => {
+        let data = JSON.parse(JSON.stringify(result));
+        let query = 'SELECT name FROM common_code \
+                    WHERE value = ? \
+                    AND pid = (SELECT id FROM common_code WHERE name=?)'
+        conn.query(query, [data.category, 'CATEGORY'], 
+            (err, results) => {
+                if (err) throw err;
+                let rows = JSON.parse(JSON.stringify(results[0]));
+                let categoryName = rows.name;
+
+                conn.query(query, [data.label, 'LABEL'], 
+                    (err, results) => {
+                        if (err) throw err;
+                        let rows = JSON.parse(JSON.stringify(results[0]));
+                        let labelName = rows.name;
+                        
+                        data['category'] = categoryName;
+                        data['label'] = labelName;
+                        resolve(data);
+                    });
+            })
+    });
+}
+
+async function getResult(results, conn) {
+    const data = await Promise.all(
+        results.map(result => {
+            return getNameByCode(result, conn);
+        })
+        
+    );
+    return data;
+}
+
 router.get('/getLabel', (req, res) => {
-    let categoryName;
-    let labelName;
-    let datas;
     getConnection((conn) => {
         conn.query('SELECT * FROM item WHERE status = 2', 
-            (err, result) => {
+            (err, results) => {
                 if(err) throw err;
-                // 각각의 row에 대해 label값, category값 대체 필요
-                datas = JSON.parse(JSON.stringify(result[0]));
-                let query = 'SELECT name FROM common_code \
-                            WHERE value = ? \
-                            AND pid = (SELECT id FROM common_code WHERE name=?)'
-                conn.query(query, [datas.category, 'CATEGORY'], 
-                    (err, result) => {
-                        if (err) throw err;
-                        let rows = JSON.parse(JSON.stringify(result[0]));
-                        categoryName = rows.name;
+                
+                getResult(results, conn).then((data) => {
+                    res.json(data);
+                })
+            })
 
-                        conn.query(query, [datas.label, 'LABEL'], 
-                            (err, result) => {
-                                if (err) throw err;
-                                let rows = JSON.parse(JSON.stringify(result[0]));
-                                labelName = rows.name;
-                                
-                                datas['category'] = categoryName;
-                                datas['label'] = labelName;
-                                console.log('type', typeof(datas))
-
-                            })
-                    })
-                res.json(result);
-            });
-    });
+        conn.release();
+    })
+   
 });
+
+
+/* 
+    DO VERIFY
+    POST /api/label/doVerify
+*/
+router.post('/doVerify', (req, res) => {
+    let files = req.body.files; // itemID, label, comment
+    let userID = req.session.loginInfo.uid;
+    getConnection((conn) => {
+        conn.query('SELECT value FROM common_code WHERE name = ?', [files.label],
+            (err, result) => {
+                if(err)  throw err;
+                let rows = JSON.parse(JSON.stringify(result[0]));
+                let labelCode = rows.value; 
+
+                conn.query('INSERT INTO review (item_id, label, comment, uploader_uid, reviewer_uid) VALUES (?, ?, ?, ?, ?)', 
+                    [files.itemID, labelCode, files.comment, files.uploader_uid, userID], 
+                    (err) => {
+                        if(err) throw err;
+                    })
+                conn.query('UPDATE item SET status = ? WHERE (id = ?)' , [3, files.itemID], 
+                    (err, result) => {
+                        if(err) throw err;
+                        return res.status(200).json({ success: true });
+                    })
+            })
+        conn.release();
+    })
+})
 
 export default router;
