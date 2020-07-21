2e9e63b4db5ca065df76b20343535085d787d64b
minji Song <ssong758@gmail.com>
2020-07-21 10:12:54 +0900

    implement getting verified item

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,8 @@ import SignIn from './Signin';
 import SignUp from './SignUp';
 import LabelItem from './LabelItem';
 import LabelItemList from './LabelItemList';
+import VerifiedItem from './VerifiedItem';
+import VerifiedItemList from './VerifiedItemList';
 
-export { Header, UploadItem, SignIn, SignUp, LabelItem, LabelItemList };
+
+export { Header, UploadItem, SignIn, SignUp, LabelItem, LabelItemList, VerifiedItem, VerifiedItemList };
